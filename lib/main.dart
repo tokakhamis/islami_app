2f5c635a0f72e2552fcdd36df33dbc1ca872043d
tokakhamis <ta472660@gmail.com>
2024-07-18 03:07:04 +0300

    Initial project. added empty screen.

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:islami_project/home_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: HomeScreen.routName,
+      routes: {HomeScreen.routName: (context) => HomeScreen()},
+    );
+  }
+}
